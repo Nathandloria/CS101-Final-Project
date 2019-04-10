c6dc8d3e8c74ef9687dc22d5bf7d3f6cc1ad799f
Adam Cook <adam@Adams-MacBook-Pro-15.local>
2019-04-10 15:02:24 -0400

    added stringEncription

---
+++ src/stringEncryption.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
+
+/** This is a simple encrption method to encript a string by shifting it 100
+*   characters by the acii chart.
+*   @author Adam Cook
+*/
+
+public class stringEncryption{
+
+  public void encript(String message){
+    char[] a = message.toCharArray();
+      int key = 150;  // Shift key for CaesarCipher
+      // Encripts by shifting position of each char by key
+      for (int i=0; i<message.length(); i++) {
+        char temp = a[i];
+        int numTemp = temp;
+        int encriptedNumTemp = (numTemp + key);
+        a[i] = (char)encriptedNumTemp;
+      }
+      //System.out.println(Arrays.toString(a)); //Displays encripted message
+      return Arrays.toString(a);
+    }
+  }
